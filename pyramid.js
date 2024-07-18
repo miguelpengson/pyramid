d463a4047825ade5d69f135b6ee46c85cd03764b
L. Miguel H. Pengson <28564113+miguelpengson@users.noreply.github.com>
2024-07-18 16:40:13 +0800

    Create pyramid.js
    initial

---
+++ pyramid.js
@@ -0,0 +1,10 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+fundtion padRow(rownNumber, rowCount) {  
+    return character.repeat(rowNumber);
+}
+for (let i = 0; i < 0; i = i + 1) {
+    rows.push(padRow(i + 1, count));
+}
