5b934b7198658fc332fcb66655ef129f3165ee2f
L. Miguel H. Pengson <28564113+miguelpengson@users.noreply.github.com>
2024-07-21 20:35:10 +0000

    pyramid generated

--- pyramid.js
+++ pyramid.js
@@ -2,9 +2,16 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-fundtion padRow(rownNumber, rowCount) {  
-    return character.repeat(rowNumber);
+function padRow(rowNumber, rowCount) {  
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 0; i < 0; i = i + 1) {
+
+
+for (let i = 0; i < count; i++) {
     rows.push(padRow(i + 1, count));
 }
+let result = "";
+for (const row of rows) {
+    result = result + "\n" + row;
+}
+console.log(result);
