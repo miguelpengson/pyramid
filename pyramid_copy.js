cafaa12f6c9a0624af1767ff704c6c130e582656
miguelpengson <miguelpengson@gmail.com>
2024-08-09 19:56:59 +0800

    added variables and for loops to create pyramid_copy

--- pyramid_copy.js
+++ pyramid_copy.js
@@ -1,17 +1,17 @@
 const character = "#";
 const count = 8;
 const rows = [];
+function padRow() {
 
-function padRow(rowNumber, rowCount) {  
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i + 1))
 }
 
+let result = '';
 
-for (let i = count; i > 0; i--) {
-    rows.push(padRow(i, count));
+for (const row of rows) {
+    result = result + '\n' + row;
 }
-let result = "";
- for (const row of rows) {
-    result = result + "\n" + row;}
+
 console.log(result);
-
